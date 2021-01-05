63a307ab69b4764cc21015ef774415d76a2ff5bd
Philippe <pslessard@gmail.com>
2021-01-05 15:53:22 -0500

    Adds two commands:   promote - Admin only. Moves a player to a different place in the 	    queue. Takes a name and optionally a position. Position 	    defaults to 1   add	  - Admin only. Adds a player to the queue. Takes a name

--- discord_bot.py
+++ discord_bot.py
@@ -77,6 +77,30 @@ async def next_player(ctx):
     else:
         await ctx.send(f"Up next: {player}")
 
+# Command to move someone to a different part of the queue. Can only be done by people with the Admin role
+@bot.command(name='promote', help='Moves a player to a different position in the queue. Can only be used by admins')
+@commands.has_any_role(*admin_roles)
+async def promote_player(ctx, name, position=1):
+    global queue
+
+    success = queue.promote(name, position)
+    if success:
+        await ctx.send(f"{name} is now in position {position} in the queue")
+    else:
+        await ctx.send(f"Unable to update queue")
+
+# Command to add someone else to the queue. Can only be done by people with the Admin role
+@bot.command(name='add', help='Adds specified player to the queue. Can only be used by admins')
+@commands.has_any_role(*admin_roles)
+async def add_player(ctx, name):
+    global queue
+
+    success = queue.push(name)
+    if pos > -1:
+        await ctx.send(f"{name} has been added to the queue at position {pos}")
+    else:
+        await ctx.send(f"{name} is already in the queue")
+
 # Command to clear the queue
 @bot.command(name='clear', help='Clears the queue. Can only be used by admins')
 @commands.has_any_role(*admin_roles)
