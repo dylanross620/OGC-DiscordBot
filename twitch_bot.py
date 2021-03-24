5fa1ac2db5835288eb233fa75f19df0276c0c54a
Dylan Ross <dylanross620@gmail.com>
2021-03-24 14:24:56 -0400

    Fixed bot names again. I need to make this local someday

--- twitch_bot.py
+++ twitch_bot.py
@@ -1,7 +1,7 @@
 import irc.bot
 
-NAME = 'Radnor0'
-OWNER = 'Radnor0'
+NAME = 'ClossiBot'
+OWNER = 'Clossius'
 
 admin_badges = set(['broadcaster', 'moderator'])
 supporter_badges = set(admin_badges.union(['subscriber']))
